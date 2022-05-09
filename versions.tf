37a9e6cf09fc364d350140c57fa9f73ef21f6f14
Calvine Otieno <nyarangaotieno@gmail.com>
2022-05-09 16:39:21 +0300

    chore: add terraform required  providers plus versions
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

---
+++ versions.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = "1.1.9"
+
+  backend "s3" {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.11.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.11.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.5.1"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.7.2"
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {} # used for accesing Account ID and ARN
+
+provider "aws" {
+  region = "eu-west-1"
+  default_tags {
+    tags = {
+      iac_environment = var.iac_environment_tag
+    }
+  }
+}
