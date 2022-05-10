b9bcc6fd9d47ba06c294e5a1aedc045d673a36cd
Calvine Otieno <nyarangaotieno@gmail.com>
2022-05-10 14:44:09 +0300

    feat: define terraform configurations, related to version, providers and state file backend
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

---
+++ base/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = "1.1.9"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.11.0" # Optional but recommended in production
+    }
+  }
+
+}
