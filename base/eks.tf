8c734b4ff58f8bd1553b4e9820274dcb3e569c9c
Calvine Otieno <nyarangaotieno@gmail.com>
2022-06-20 17:41:30 +0300

    chore: add comment for eks module creation
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- base/eks.tf
+++ base/eks.tf
@@ -1,4 +1,4 @@
-module "cluster" {
+module "eks-cluster" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 18.20.2"
 
@@ -11,6 +11,7 @@ module "cluster" {
   eks_managed_node_groups         = var.eks_managed_node_groups
 
   node_security_group_additional_rules = {
+    # If you omit this, you will get Internal error occurred: failed calling webhook, the server could not find the requested resource
     # https://github.com/kubernetes-sigs/aws-load-balancer-controller/issues/2039#issuecomment-1099032289
     ingress_allow_access_from_control_plane = {
       type                          = "ingress"
@@ -18,6 +19,7 @@ module "cluster" {
       from_port                     = 9443
       to_port                       = 9443
       source_cluster_security_group = true
+      description = "Allow access from control plane to webhook port of AWS load balancer controller"
     }
     # allow connections from ALB security group
     ingress_allow_access_from_alb_sg = {
