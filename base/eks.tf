f38b5a075c0db57bec125ab8233c7c05e8cb86d0
Calvine Otieno <nyarangaotieno@gmail.com>
2022-06-20 17:48:38 +0300

    chore: add links to module sources for better documentation
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- base/eks.tf
+++ base/eks.tf
@@ -50,6 +50,7 @@ module "eks-cluster" {
 }
 
 # IAM role for AWS Load Balancer Controller, and attach to EKS OIDC
+# https://registry.terraform.io/modules/terraform-aws-modules/iam/aws/latest/submodules/iam-role-for-service-accounts-eks
 module "eks_ingress_iam" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 4.22.0"
@@ -58,7 +59,7 @@ module "eks_ingress_iam" {
   attach_load_balancer_controller_policy = true
 
   oidc_providers = {
-    ex = {
+    main = {
       provider_arn               = module.cluster.oidc_provider_arn
       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
     }
@@ -66,6 +67,7 @@ module "eks_ingress_iam" {
 }
 
 # IAM role for External DNS, and attach to EKS OIDC
+# https://registry.terraform.io/modules/terraform-aws-modules/iam/aws/latest/submodules/iam-role-for-service-accounts-eks
 module "eks_external_dns_iam" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 4.22.0"
@@ -75,14 +77,14 @@ module "eks_external_dns_iam" {
   external_dns_hosted_zone_arns = ["arn:aws:route53:::hostedzone/*"]
 
   oidc_providers = {
-    ex = {
+    main = {
       provider_arn               = module.cluster.oidc_provider_arn
       namespace_service_accounts = ["kube-system:external-dns"]
     }
   }
 }
 
-# set spot fleet and on-demand Autoscaling policy
+# Set spot fleet and on-demand Autoscaling policy
 resource "aws_autoscaling_policy" "eks_autoscaling_policy" {
   count = length(var.eks_managed_node_groups)
 
