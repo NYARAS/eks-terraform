25383b3c81d8764b655e417b8a4cda4460c23004
Calvine Otieno <nyarangaotieno@gmail.com>
2024-09-25 22:09:44 +0300

    chore: update vpc configs to latest module
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

---
+++ vpc/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of public subnet IDs"
+  value       = module.vpc.public_subnets
+}
---
+++ vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
