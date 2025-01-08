020468f9c4373d47e605b6d8e941243050e05799
Calvine Otieno <nyarangaotieno@gmail.com>
2025-01-08 22:10:40 +0300

    chore: update versions
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- vpc/main.tf
+++ vpc/main.tf
@@ -7,8 +7,7 @@ data "aws_availability_zones" "available" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.19.0"
-
+  version = "5.2.0"
   name = var.vpc_name
   cidr = "10.0.0.0/16"
 
