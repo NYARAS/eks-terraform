aa2e1c56597fc7cc4d056fa1de329f2a3672a69a
Calvine Otieno <nyarangaotieno@gmail.com>
2025-01-08 22:13:49 +0300

    chore: assorted changes
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- vpc/versions.tf
+++ vpc/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.73"
+      version = ">= 5.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
