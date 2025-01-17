ab190aa461bf3f8735ace0eac35c343daadf2960
Calvine Otieno <nyarangaotieno@gmail.com>
2025-01-17 21:56:38 +0300

    chore: format vpc config
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- vpc/main.tf
+++ vpc/main.tf
@@ -8,8 +8,8 @@ data "aws_availability_zones" "available" {
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.2.0"
-  name = var.vpc_name
-  cidr = "10.0.0.0/16"
+  name    = var.vpc_name
+  cidr    = "10.0.0.0/16"
 
   azs             = slice(data.aws_availability_zones.available.names, 0, 3)
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
