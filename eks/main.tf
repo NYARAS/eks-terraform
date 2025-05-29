3456b36a56e4d13109e08ad39d0728f421fd114e
Calvine Otieno <nyarangaotieno@gmail.com>
2025-05-29 23:03:20 +0300

    chore: cleanup to setup minimal eks cluster
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- eks/main.tf
+++ eks/main.tf
@@ -1,86 +1,32 @@
 module "eks" {
-  source                         = "terraform-aws-modules/eks/aws"
-  version                        = "20.8.4"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20"
+
   cluster_name                   = var.cluster_name
   cluster_version                = "1.32"
   cluster_endpoint_public_access = true
-  create_iam_role                = false
-  iam_role_arn                   = aws_iam_role.eks-role.arn
+  # IPV6
+  cluster_ip_family          = var.cluster_ip_family
+  create_cni_ipv6_iam_policy = var.cluster_ip_family == "ipv6" ? true : false
+  vpc_id                  = module.vpc.vpc_id
+  subnet_ids         = module.vpc.private_subnets
 
-  cluster_enabled_log_types   = []
-  create_cloudwatch_log_group = false
+  # Fargate profiles use the cluster primary security group so these are not utilized
+  create_cluster_security_group = false
+  create_node_security_group    = false
 
-  enable_irsa                              = true
   enable_cluster_creator_admin_permissions = true
-  authentication_mode                      = "API_AND_CONFIG_MAP"
-  vpc_id                                   = var.vpc_id
-  subnet_ids                               = var.private_subnets
-  control_plane_subnet_ids                 = var.private_subnets
-
-  tags = {
-    cluster = "demo"
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
   }
-
-  cluster_addons = {
-    coredns = {
-      resolve_conflicts = "OVERWRITE"
-    }
-    eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    vpc-cni = {
-      resolve_conflicts = "OVERWRITE"
-    }
+  cluster_upgrade_policy = {
+    support_type = "STANDARD"
   }
+  access_entries = {}
 
-  # eks_managed_node_group_defaults = {
-  #   ami_type       = "AL2_x86_64"
-  #   instance_types = ["t3.medium"]
-  # }
-
-  eks_managed_node_groups = var.eks_managed_node_groups
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.eks-AmazonEKSVPCResourceController,
-  ]
-
-  cluster_security_group_additional_rules = {
-    ingress_node_vault_port = {
-      description                = "Vault Port"
-      protocol                   = "tcp"
-      from_port                  = 8200
-      to_port                    = 8200
-      type                       = "ingress"
-      source_node_security_group = true
-    }
-  }
-
-}
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "eks-role" {
-  name               = "eks-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.eks-role.name
+  # enable_irsa = true
+  tags = merge(local.tags, {
+    "karpenter.sh/discovery" = var.cluster_name
+  })
 }
--- eks/variables.tf
+++ eks/variables.tf
@@ -1,19 +1,46 @@
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
+  default     = "dev-eit"
+  description = "Name of the Cluster"
   type        = string
 }
-
-variable "vpc_id" {
-  description = "ID of the VPC"
+variable "cluster_ip_family" {
+  default     = "ipv4"
+  description = "IP Family"
   type        = string
 }
-
-variable "private_subnets" {
-  description = "List of private subnet IDs"
-  type        = list(string)
+variable "env" {
+  default     = "dev"
+  description = "Name of the environment"
+  type        = string
 }
-
-variable "eks_managed_node_groups" {
-  type        = map(any)
-  description = "Map of EKS managed node group definitions to create"
+variable "vpc_block" {
+  default     = "172.16.0.0/16"
+  description = "VPC CIDR Block"
+  type        = string
+}
+variable "saml_role_arn" {
+  default = "arn:aws:iam::652750967253:role/global-DeptAdmin-1WL9N53EKDOP8"
+  type    = string
+}
+variable "dns_zone" {
+  description = "DNS Zone"
+  type        = string
+  default     = "calvineotieno.com"
+}
+data "aws_route53_zone" "eks" {
+  name = var.dns_zone
+}
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+data "aws_eks_cluster_auth" "default" {
+  name = var.cluster_name
+}
+locals {
+  tags = {
+    Environment   = var.env
+    Project       = "kube"
+    Managed       = "true"
+    ManagedBy     = "Terraform"
+    TerraformRepo = "https://code.umd.edu/eit/development/kube"
+  }
 }
