fb8fb0ff8ff47e6ad2a9ff0e827217f17b2086d2
Calvine Otieno <nyarangaotieno@gmail.com>
2025-05-30 19:22:40 +0300

    chore: cluster config update
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- eks/variables.tf
+++ eks/variables.tf
@@ -1,8 +1,29 @@
 variable "cluster_name" {
-  default     = "dev-eit"
-  description = "Name of the Cluster"
+  description = "Name of the EKS cluster"
   type        = string
 }
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
+
+variable "eks_managed_node_groups" {
+  type        = map(any)
+  description = "Map of EKS managed node group definitions to create"
+}
+
+variable "cluster_version" {
+  type        = string
+  description = "Version of the eks cluster."
+  default     = "1.32"
+}
+
 variable "cluster_ip_family" {
   default     = "ipv4"
   description = "IP Family"
@@ -12,35 +33,4 @@ variable "env" {
   default     = "dev"
   description = "Name of the environment"
   type        = string
-}
-variable "vpc_block" {
-  default     = "172.16.0.0/16"
-  description = "VPC CIDR Block"
-  type        = string
-}
-variable "saml_role_arn" {
-  default = "arn:aws:iam::652750967253:role/global-DeptAdmin-1WL9N53EKDOP8"
-  type    = string
-}
-variable "dns_zone" {
-  description = "DNS Zone"
-  type        = string
-  default     = "calvineotieno.com"
-}
-data "aws_route53_zone" "eks" {
-  name = var.dns_zone
-}
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-data "aws_eks_cluster_auth" "default" {
-  name = var.cluster_name
-}
-locals {
-  tags = {
-    Environment   = var.env
-    Project       = "kube"
-    Managed       = "true"
-    ManagedBy     = "Terraform"
-    TerraformRepo = "https://code.umd.edu/eit/development/kube"
-  }
 }
