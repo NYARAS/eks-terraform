d8f4db06844518cb44ad0b9287ada95f8cb40b02
Calvine Otieno <nyarangaotieno@gmail.com>
2025-06-01 23:26:10 +0300

    chore: update eks config
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- eks/main.tf
+++ eks/main.tf
@@ -3,74 +3,54 @@ module "eks" {
   version = "~> 20"
 
   cluster_name                   = var.cluster_name
-  cluster_version                = var.cluster_version
+  cluster_version                = "1.32"
   cluster_endpoint_public_access = true
   # IPV6
   cluster_ip_family          = var.cluster_ip_family
   create_cni_ipv6_iam_policy = var.cluster_ip_family == "ipv6" ? true : false
   vpc_id                     = var.vpc_id
   subnet_ids                 = var.private_subnets
-  create_iam_role            = false
-  iam_role_arn               = aws_iam_role.eks-role.arn
 
+  # Fargate profiles use the cluster primary security group so these are not utilized
   create_cluster_security_group = false
   create_node_security_group    = false
 
   enable_cluster_creator_admin_permissions = true
   eks_managed_node_groups                  = var.eks_managed_node_groups
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
   cluster_upgrade_policy = {
     support_type = "STANDARD"
   }
-  access_entries = {
+  access_entries = {}
 
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      most_recent       = true
+      resolve_conflicts = "OVERWRITE"
+    }
+    coredns = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent    = true
+      before_compute = true
+      configuration_values = jsonencode({
+        env = {
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
+    }
+    kube-proxy = { most_recent = true }
+    eks-pod-identity-agent = {
+      most_recent = true
+    }
   }
-  # enable_irsa = true
+  enable_irsa = true
   tags = merge(local.tags, {
     "karpenter.sh/discovery" = var.cluster_name
   })
-}
-
-module "vpc_cni_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.55.0"
-
-  role_name_prefix      = "VPC-CNI-IRSA"
-  attach_vpc_cni_policy = true
-  vpc_cni_enable_ipv4   = true # NOTE: This was what needed to be added
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-node"]
-    }
-  }
-}
-
-resource "aws_iam_role" "eks-role" {
-  name               = "eks-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.eks-role.name
-}
-
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
 }
