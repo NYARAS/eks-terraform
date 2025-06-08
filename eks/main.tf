004335a45654b70da0ae398de4438b9b8f63bbd5
Calvine Otieno <nyarangaotieno@gmail.com>
2025-06-08 19:07:01 +0300

    chore: add managed nodegroup config
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- eks/main.tf
+++ eks/main.tf
@@ -1,25 +1,22 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20"
+  version = "~> 20.24"
 
   cluster_name                   = var.cluster_name
-  cluster_version                = "1.32"
+  cluster_version                = "1.30"
   cluster_endpoint_public_access = true
   # IPV6
   cluster_ip_family          = var.cluster_ip_family
   create_cni_ipv6_iam_policy = var.cluster_ip_family == "ipv6" ? true : false
-  vpc_id                     = var.vpc_id
-  subnet_ids                 = var.private_subnets
+  vpc_id                     = module.vpc.vpc_id
+  subnet_ids                 = module.vpc.private_subnets
 
   # Fargate profiles use the cluster primary security group so these are not utilized
   create_cluster_security_group = false
   create_node_security_group    = false
 
   enable_cluster_creator_admin_permissions = true
-  eks_managed_node_groups                  = var.eks_managed_node_groups
-  cluster_upgrade_policy = {
-    support_type = "STANDARD"
-  }
+
   access_entries = {
   }
 
@@ -47,6 +44,21 @@ module "eks" {
     }
   }
   enable_irsa = true
+  eks_managed_node_groups = {
+    eks_mng = {
+      max_size       = 3
+      min_size       = 3
+      desired_size   = 3
+      instance_types = ["t4g.medium"]
+      ami_type       = "BOTTLEROCKET_ARM_64"
+      # key_name         = var.key_name
+      subnets = module.vpc.private_subnets
+      update_config = {
+        max_unavailable = 1
+      }
+    }
+  }
+
   tags = merge(local.tags, {
     "karpenter.sh/discovery" = var.cluster_name
   })
