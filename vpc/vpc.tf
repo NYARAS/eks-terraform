9b4cf765ea1f45159cc2e820274b06054826bcad
Calvine Otieno <nyarangaotieno@gmail.com>
2025-08-20 22:39:55 +0300

    chore: remove old code
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -5,36 +5,6 @@ data "aws_availability_zones" "available" {
   }
 }
 
-# module "vpc" {
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "5.2.0"
-#   name    = var.vpc_name
-#   cidr    = "10.0.0.0/16"
-
-#   azs             = slice(data.aws_availability_zones.available.names, 0, 3)
-#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
-#   enable_nat_gateway   = true
-#   single_nat_gateway   = true
-#   enable_dns_hostnames = true
-
-#   public_subnet_tags = {
-#     # "kubernetes.io/cluster/${var.cluster_name}" = "owned"
-#     "kubernetes.io/role/elb" = "1"
-#   }
-
-#   private_subnet_tags = {
-#     # "kubernetes.io/cluster/${var.cluster_name}" = "owned"
-#     "kubernetes.io/role/internal-elb" = "1"
-#   }
-# }
-
-# variable "vpc_name" {
-#   description = "Name of the VPC"
-#   type        = string
-# }
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
